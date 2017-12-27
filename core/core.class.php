c2aeebf093d78f923371a1275ebb0cdaf9282054
xhwan123 <275657684@qq.com>
2017-12-27 13:59:58 +0800

    第一次添加

---
+++ core/core.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Kang;
+
+class Core{
+
+	public static function run(){
+		echo "me 第一个 composer自动加载";
+	}
+}
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+include "vendor/autoload.php";
+// include "core/core.class.php";
+
+ini_set('display_errors','On');
+error_reporting(E_ALL);
+
+// // echo __DIR__."vendor/autoload.php";
+Kang\Core::run();
+
+
+
+
+
+?>
